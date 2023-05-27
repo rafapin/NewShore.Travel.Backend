9282d54d995303703d5b2f4813bf42652266fa0f
Rafael Alexis Pinto Florez <rafael.pintof@opitech.com.co>
2023-05-26 21:03:05 -0500

    implementaion console log

--- Application/NewShore.Travel.Infrastructure/Config/Config.cs
+++ Application/NewShore.Travel.Infrastructure/Config/Config.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using NewShore.Travel.Application.Contracts.Logger;
 using NewShore.Travel.Domain.Features.Flights.Contracts.Integrations;
 using NewShore.Travel.Infrastructure.Integrations.Flights.NewShore;
+using NewShore.Travel.Infrastructure.Logger;
 
 namespace NewShore.Travel.Infrastructure.Config
 {
@@ -9,6 +11,7 @@ namespace NewShore.Travel.Infrastructure.Config
         public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
         {
             services.AddScoped(typeof(IFlightsService), typeof(NewShoreFlightService));
+            services.AddSingleton(typeof(ICustomLogger<>), typeof(ConsoleLog<>));
             return services;
         }
     }
---
+++ Application/NewShore.Travel.Infrastructure/Logger/ConsoleLog.cs
@@ -0,0 +1,34 @@
+﻿using NewShore.Travel.Application.Contracts.Logger;
+using System;
+using System.Text.Json;
+
+namespace NewShore.Travel.Infrastructure.Logger
+{
+    internal class ConsoleLog<T> : ICustomLogger<T> where T : class
+    {
+        public void LogError(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void LogInformation<TInput>(TInput message)
+        {
+            Console.Write(JsonSerializer.Serialize(message));
+        }
+
+        public void LogInformation(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void LogWarning<TInput>(TInput message)
+        {
+            Console.Write(JsonSerializer.Serialize(message));
+        }
+
+        public void LogWarning(string message)
+        {
+            Console.Write(message);
+        }
+    }
+}
